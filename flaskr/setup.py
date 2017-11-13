88a4a68eada8b77304e953ea59f9abfdd700b614
Bernardc Paulus <bprecyclebin@gmail.com>
2017-11-13 14:54:17 +0100

    allow running tests via setup.py

--- flaskr/setup.py
+++ flaskr/setup.py
@@ -5,5 +5,7 @@ setup(
     name='flaskr',
     packages=['flaskr'],
     include_package_data=True,
-    install_requires=['flask']
+    install_requires=['flask'],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest']
 )
